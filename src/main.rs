7e9de4af7c0850042c81c2296a7e2b41bff10809
Hermann Schwarz <schwarz@dipf.de>
2020-10-31 21:20:35 +0100

    Working version with erroro handling "net::ERR_NAME_NOT_RESOLVE"

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs;
 use std::path::Path;
 use std::time::Instant;
-use failure::Fallible;
+use failure::{Fallible};
+use url::{Url};
 use headless_chrome::{Browser, protocol::{target::methods::CreateTarget, page::ScreenshotFormat}};
 
 use mysql::Pool;
@@ -17,19 +18,30 @@ fn main() -> Fallible<()> {
 
     let start_time = Instant::now();
 
-    let bookmarks = select_db();
-
-    // Create a headless browser, navigate to wikipedia.org, wait for the page
-    // to render completely, take a screenshot of the entire page
-    // in JPEG-format using 75% quality.
-
     let width = 1024;
     let height = 800;
-
     let browser = Browser::default()?;
 
+    let mut bookmarks = select_db();
 
-    for bookmark in bookmarks.iter() {
+    for bookmark in bookmarks.iter_mut() {
+
+        let url_size = bookmark.edutags_url.len();
+        let file_ext = &bookmark.edutags_url[url_size-3..];
+        let url = Url::parse(&bookmark.edutags_url)?;
+
+        match file_ext == "pdf" {
+            true => {
+                match base_url(url) {
+                    Ok(v) => bookmark.edutags_url = String::from(v.as_str()),
+                    Err(e) => println!("base_url ERROR: {:?}", e)
+                }
+            }
+            _ => {
+                
+            }
+        }
+
         println!("BID: {}, URL: {}", bookmark.bid, bookmark.edutags_url);
     
         //    let tab = browser.wait_for_initial_tab()?;
@@ -39,24 +51,20 @@ fn main() -> Fallible<()> {
             height: Some(height.into()),
             browser_context_id: None,
             enable_begin_frame_control: None,
-        })?;  
-
-        println!("Tab created");
-
-        /*
-        let jpeg_data = tab
-            .navigate_to("https://www.wikipedia.org")?
-            .wait_until_navigated()?
-            .capture_screenshot(ScreenshotFormat::JPEG(Some(75)), None, true)?;
-        fs::write("screenshot.jpg", &jpeg_data)?;
-        */ 
+        })?; 
 
         // Browse to the WebKit-Page and take a screenshot of the infobox.
-        let png_data = tab
-        //    .navigate_to(&bookmark.edutags_url)?
-        //   .wait_for_element("#mw-content-text > div > table.infobox.vevent")?
-            .wait_until_navigated()?
-            .capture_screenshot(ScreenshotFormat::PNG, None, true)?;
+        match tab.navigate_to(&bookmark.edutags_url) {
+            Ok(tab_nav) => tab_nav,
+            Err(e) => {
+                println!("URL is not reachable. Error {}", e);
+                continue;
+            },
+        };
+        
+        tab.wait_until_navigated()?;
+
+        let png_data = tab.capture_screenshot(ScreenshotFormat::PNG, None, true)?;
 
         let mut filename: String = bookmark.bid.to_string().to_owned();
         let file_ext: &str = ".png";
@@ -84,7 +92,7 @@ fn select_db() -> Vec<Bookmark> {
 
 
     let bookmarks: Vec<Bookmark> = pool.prep_exec(
-        "SELECT bid AS bid, url AS edutagsURL FROM bookmark WHERE url IS NOT NULL AND url <> '' AND bid >= 12 AND bid <= 20", ())
+        "SELECT bid AS bid, url AS edutagsURL FROM bookmark WHERE url IS NOT NULL AND url <> '' AND bid >= 1 AND bid <= 20", ())
         .map(|result| {
             result.map(|x| x.unwrap())
             .map(|row| {
@@ -98,4 +106,20 @@ fn select_db() -> Vec<Bookmark> {
         }).unwrap();
 
     return bookmarks;
+}
+
+
+fn base_url(mut url: Url) -> Result<Url, &'static str> {
+    match url.path_segments_mut() {
+        Ok(mut path) => {
+            path.clear();
+        }
+        Err(_) => {
+            return Err("Could not shorten URL");
+        }
+    }
+
+    url.set_query(None);
+
+    Ok(url)
 }
